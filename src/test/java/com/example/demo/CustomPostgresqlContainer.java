c8842196401dda12d225b90f1905ae8ebd389404
aridwiprayogo <kisamberbledek@yahoo.com>
2020-09-11 18:43:34 +0700

    update testcontainer still error

--- src/test/java/com/example/demo/CustomPostgresqlContainer.java
+++ src/test/java/com/example/demo/CustomPostgresqlContainer.java
@@ -1,32 +1,38 @@
 package com.example.demo;
 
+import org.junit.ClassRule;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.containers.PostgreSQLContainer;
 
-public class CustomPostgresqlContainer extends PostgreSQLContainer<CustomPostgresqlContainer> {
-    private static final String IMAGE_VERSION = "postgres:11.1";
-    private static CustomPostgresqlContainer container;
 
-    private CustomPostgresqlContainer() {
-        super(IMAGE_VERSION);
-    }
+@ContextConfiguration(initializers = {CustomPostgresqlContainer.Initializer.class})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public abstract class CustomPostgresqlContainer {
 
-    public static CustomPostgresqlContainer getInstance() {
-        if (container == null) {
-            container = new CustomPostgresqlContainer();
+    @ClassRule
+    private static final PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres:12.4")
+                    .withDatabaseName("postgres")
+                    .withUsername("postgres")
+                    .withPassword("root");
+
+    public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @Override
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            postgreSQLContainer.start();
+            TestPropertyValues.of(
+                    "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
+                    "spring.datasource.username=" + postgreSQLContainer.getUsername(),
+                    "spring.datasource.password=" + postgreSQLContainer.getPassword()
+            ).applyTo(configurableApplicationContext.getEnvironment());
         }
-        return container;
-    }
-
-    @Override
-    public void start() {
-        super.start();
-        System.setProperty("DB_URL", container.getJdbcUrl());
-        System.setProperty("DB_USERNAME", container.getUsername());
-        System.setProperty("DB_PASSWORD", container.getPassword());
-    }
-
-    @Override
-    public void stop() {
-        //do nothing, JVM handles shut down
     }
 }
--- src/test/java/com/example/demo/repository/NewsRepositoryTest.java
+++ src/test/java/com/example/demo/repository/NewsRepositoryTest.java
@@ -1,29 +1,35 @@
 package com.example.demo.repository;
 
+import com.example.demo.CustomPostgresqlContainer;
 import com.example.demo.domain.News;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @DataJpaTest
 @RunWith(SpringRunner.class)
-@ContextConfiguration(initializers = {UserRepositoryTCIntegrationTest.Initializer.class})
-class NewsRepositoryTest {
+@ExtendWith(SpringExtension.class)
+class NewsRepositoryTest extends CustomPostgresqlContainer{
     @Autowired
     private NewsRepository repository;
     @Autowired
     private TestEntityManager entityManager;
+
     @Test
     void findAll(){
         final List<News> newsList = this.repository.findAll();
